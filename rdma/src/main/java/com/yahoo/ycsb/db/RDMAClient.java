068e1d03d1afd061b7934a767f69593796a85f03
mazx <dyxdy@live.com>
2018-09-09 20:59:22 +0800

    fix format

--- rdma/src/main/java/com/yahoo/ycsb/db/RDMAClient.java
+++ rdma/src/main/java/com/yahoo/ycsb/db/RDMAClient.java
@@ -1,3 +1,27 @@
+/**
+ * Copyright (c) 2012 YCSB contributors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+
+/**
+ * Redis client binding for YCSB.
+ *
+ * All YCSB records are mapped to a Redis *hash field*.  For scanning
+ * operations, all keys are saved (by an arbitrary hash) in a sorted set.
+ */
+
 package com.yahoo.ycsb.db;
 
 import com.yahoo.ycsb.ByteIterator;
@@ -24,68 +48,72 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 
+/**
+ * YCSB binding for <a href="http://redis.io/">Redis</a>.
+ *
+ * See {@code redis/README.md} for details.
+ */
 public class RDMAClient extends DB {
 
-	//use fifo for Redis-cli
-	//private JedisCommands jedis;
+    //use fifo for Redis-cli
+    //private JedisCommands jedis;
 
-	public static final String HOST_PROPERTY = "redis.host";
-	public static final String PORT_PROPERTY = "redis.port";
-	//public static final String PASSWORD_PROPERTY = "redis.password";
-	//public static final String CLUSTER_PROPERTY = "redis.cluster";
+    public static final String HOST_PROPERTY = "redis.host";
+    public static final String PORT_PROPERTY = "redis.port";
+    //public static final String PASSWORD_PROPERTY = "redis.password";
+    //public static final String CLUSTER_PROPERTY = "redis.cluster";
 
-	//public static final String INDEX_KEY = "_indices";
+    //public static final String INDEX_KEY = "_indices";
 
-	public void init() throws DBException {
-		Properties props = getProperties();
-		String host = props.getProperty(HOST_PROPERTY);
-		String portString = props.getProperty(PORT_PROPERTY);
-		int port = Integer.parseInt(portString);
-	}
+    public void init() throws DBException {
+        Properties props = getProperties();
+        String host = props.getProperty(HOST_PROPERTY);
+        String portString = props.getProperty(PORT_PROPERTY);
+        int port = Integer.parseInt(portString);
+    }
 
-	public void cleanup() throws DBException {
-		//TODO cleanup
-	}
+    public void cleanup() throws DBException {
+    }
 
-	/*
-	 * Calculate a hash for a key to store it in an index. The actual return value
-	 * of this function is not interesting -- it primarily needs to be fast and
-	 * scattered along the whole space of doubles. In a real world scenario one
-	 * would probably use the ASCII values of the keys.
-	 */
-	private double hash(String key) {
-		return key.hashCode();
-	}
+    /*
+     * Calculate a hash for a key to store it in an index. The actual return value
+     * of this function is not interesting -- it primarily needs to be fast and
+     * scattered along the whole space of doubles. In a real world scenario one
+     * would probably use the ASCII values of the keys.
+     */
+    private double hash(String key) {
+        return key.hashCode();
+    }
 
-	// XXX jedis.select(int index) to switch to `table`
+    // XXX jedis.select(int index) to switch to `table`
 
-	@Override
-	public Status read(String table, String key, Set<String> fields,
-			Map<String, ByteIterator> result) {
-		return Status.OK;
-	}
+    @Override
+    public Status read(String table, String key, Set<String> fields,
+            Map<String, ByteIterator> result) {
+        return Status.OK;
+    }
 
-	@Override
-	public Status insert(String table, String key,
-			Map<String, ByteIterator> values) {
-		return Status.OK;
-	}
+    @Override
+    public Status insert(String table, String key,
+            Map<String, ByteIterator> values) {
+        return Status.OK;
+    }
 
-	@Override
-	public Status delete(String table, String key) {
-		Status.OK;
-	}
+    @Override
+    public Status delete(String table, String key) {
+        return Status.OK;
+    }
 
-	@Override
-	public Status update(String table, String key,
-			Map<String, ByteIterator> values) {
-			return Status.OK;
-	}
+    @Override
+    public Status update(String table, String key,
+            Map<String, ByteIterator> values) {
+        return Status.OK;
+    }
 
-	@Override
-	public Status scan(String table, String startkey, int recordcount,
-			Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-		return Status.OK;
-	}
+    @Override
+    public Status scan(String table, String startkey, int recordcount,
+            Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+        return Status.OK;
+    }
 
 }
